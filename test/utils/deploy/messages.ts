ae9d0f1a141d96bcbe9fae7d3c7e5af983d48ca8
Chadwick Strange <cstrangedk@users.noreply.github.com>
2022-02-17 15:40:54 -0800

    Remove

--- test/utils/deploy/messages.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from "hardhat";
-import { MessagesTester } from "../../../typechain/MessagesTester";
-
-export async function deployMessages() {
-    const factory = await ethers.getContractFactory("MessagesTester");
-    const instance = await factory.deploy() as MessagesTester;
-    return instance;
-}
--- test/utils/deploy/test/keyStorageMock.ts
+++
@@ -1,9 +0,0 @@
-import { ethers } from "hardhat";
-import { KeyStorageMock } from "../../../../typechain";
-
-export async function deployKeyStorageMock(
-) {
-    const factory = await ethers.getContractFactory("KeyStorageMock");
-    const instance = await factory.deploy() as KeyStorageMock;
-    return instance;
-}
